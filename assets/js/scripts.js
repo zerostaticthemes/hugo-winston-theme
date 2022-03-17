5ebb6fd2502adf441725cd61317ff06aed1e6923
Robert Austin <rob@jugglerdigital.com>
2022-03-17 18:26:38 +1000

    add mobile hamburger menu

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+var body = document.querySelector('body');
+var menuTrigger = document.querySelector('#toggle-menu-main-mobile');
+var menuContainer = document.querySelector('#menu-main-mobile');
+var hamburgerIcon = document.querySelector('.hamburger');
+
+if (menuTrigger !== null) {
+  menuTrigger.addEventListener('click', function(e) {
+    menuContainer.classList.toggle('open');
+    hamburgerIcon.classList.toggle('is-active');
+    body.classList.toggle('lock-scroll');
+  });
+}
